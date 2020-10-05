e9b221aef074028b42da1e9c0f188dd4269eacf3
KARPOVTOP <coolxex86@gmail.com>
2020-10-05 14:17:39 +0300

    Initial commit

---
+++ src/ru/mirea/lab5/Solution12.java
@@ -0,0 +1,27 @@
+package ru.mirea.lab5;
+
+
+import java.util.Scanner;
+
+public class Solution12 {
+    public Solution12() {
+    }
+
+    public static void recursion() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n > 0) {
+            if (n % 2 == 1) {
+                System.out.println(n);
+                recursion();
+            } else {
+                recursion();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        recursion();
+    }
+}
